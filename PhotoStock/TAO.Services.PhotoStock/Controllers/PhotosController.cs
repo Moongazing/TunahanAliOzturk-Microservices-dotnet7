6f437ffcd03e643d85c9c66057602aaebdcbdcc8
Moongazing <72943708+Moongazing@users.noreply.github.com>
2023-04-06 01:55:26 +0300

    Adding Photo Stock Controller and Dto

---
+++ PhotoStock/TAO.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using TAO.Services.PhotoStock.DTOs;
+using TAO.Shared.BaseController;
+using TAO.Shared.DTOs;
+
+namespace TAO.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoStock(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if (photo != null && photo.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwroot/photos", photo.FileName);
+
+                var stream = new FileStream(path, FileMode.Create);
+                await photo.CopyToAsync(stream, cancellationToken);
+                var returnPath = "photos/" + photo.FileName;
+
+                var photoDto = new PhotoDto
+                {
+                    Url = returnPath
+                };
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto,200));
+                
+            }
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty.", 400));
+        }
+        [HttpDelete]
+        public async Task<IActionResult> PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("Photo not found.", 404));   
+            }
+            System.IO.File.Delete(path);
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
+    }
+}
---
+++ PhotoStock/TAO.Services.PhotoStock/DTOs/PhotoDto.cs
@@ -0,0 +1,7 @@
+﻿namespace TAO.Services.PhotoStock.DTOs
+{
+    public class PhotoDto
+    {
+        public string Url { get; set; }
+    }
+}
