3361026776c35766553117eba9d8267eb653a3b6
Moongazing <72943708+Moongazing@users.noreply.github.com>
2023-04-06 20:22:44 +0300

    Refactoring config

---
+++ IdentityServer/TAO.IdentityServer/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using TAO.IdentityServer.DTOs;
+using TAO.IdentityServer.Models;
+using TAO.Shared.DTOs;
+
+namespace TAO.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto signUpDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signUpDto.UserName,
+                Email = signUpDto.Email,
+            };
+            var result = await _userManager.CreateAsync(user, signUpDto.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+            }
+            return NoContent();
+        }
+    }
+}
